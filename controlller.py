7b6d4ad9f603ba46b7a16b5160b7c545f1c0085d
Maxim <vedernikovmaxim@rambler.ru>
2022-06-16 22:12:22 +0300

    Закончен функционал, начато отображение

--- controlller.py
+++ controlller.py
@@ -1,7 +1,9 @@
 from datetime import date
+import json
 import re
 import string
 from this import d
+
 telephone_dictionary = {}
 
 
@@ -13,14 +15,32 @@ def add_data_telephone(key):
 
 def print_string_directory(key, telephone_directory: dict):
 
+    print(data_list_parse(key, telephone_directory))
+
+
+def data_list_parse(key, telephone_directory: dict, flag='line'):
+
     data_list = telephone_directory[key].split('/')
 
-    if len(data_list) == 4:
-        print(f'{telephone_directory[key]}// {data_list[0]} - номер телефона: {number_telef(data_list[1])}; '
-              f'дата создания контакта: {data_list[-1]}, примечание: {data_list[2]}')
-    else:
-        print(f'{telephone_directory[key]}// {data_list[0]} - номер телефона: {number_telef(data_list[1])}; '
-              f'дата создания контакта: {data_list[-1]}')
+    data_string = ''
+
+    if flag == 'line':
+        if len(data_list) == 4:
+            data_string += f'{key} {data_list[0]} - номер телефона: {number_telef(data_list[1])}; '\
+                f'дата создания контакта: {data_list[-1]}, примечание: {data_list[2]}'
+        else:
+            data_string += f'{key} {data_list[0]} - номер телефона: {number_telef(data_list[1])}; '\
+                f'дата создания контакта: {data_list[-1]}'
+    
+    if flag == 'columns':
+        if len(data_list) == 4:
+            data_string += f'{key}\n \t{data_list[0]}\n \tномер телефона: {number_telef(data_list[1])}\n'\
+                f'\tдата создания контакта: {data_list[-1]}\n \tпримечание: {data_list[2]}'
+        else:
+            data_string += f'{key}\n \t{data_list[0]}\n \tномер телефона: {number_telef(data_list[1])}\n'\
+                f'\tдата создания контакта: {data_list[-1]}'
+
+    return data_string
 
 
 def number_telef(telephone_number):
@@ -44,8 +64,8 @@ def edit_data_tel(key, dictionary: dict, identific: str):
         data_list[1] = string_data
     elif identific == 'ФИО':
         data_list[0] = string_data
-    
-    if len(data_list)==4:
+
+    if len(data_list) == 4:
         if identific == 'дата':
             data_list[3] = string_data
         elif identific == 'комментарий':
@@ -59,6 +79,22 @@ def edit_data_tel(key, dictionary: dict, identific: str):
     dictionary[key] = str
 
 
+def print_in_file(data_dictionary: dict, format: str):
+    if format == '/json':
+        with open('data_dictionary.json', 'a') as file:
+            json.dump(data_dictionary, file, indent=4, ensure_ascii=False)
+    
+    elif format == '/txt_line':
+        with open('data_dictionary_line.txt', 'a') as file:
+            for key in data_dictionary:
+                data_string = data_list_parse(key, data_dictionary)
+                file.write(data_string + '\n')
+    
+    elif format == '/txt_columns':
+        with open('data_dictionary_columns.txt', 'a') as file:
+            for key in data_dictionary:
+                data_string = data_list_parse(key, data_dictionary, 'columns')
+                file.write(data_string + '\n')
 
 
 if __name__ == "__main__":
@@ -66,9 +102,11 @@ if __name__ == "__main__":
     print('ddd')
     k = input()
     add_data_telephone(k)
-    print(telephone_dictionary)
-    edit_data_tel('1', telephone_dictionary, 'комментарий')
-    print(telephone_dictionary)
+    print('ddd')
+    k = input()
+    add_data_telephone(k)
+    print_in_file(telephone_dictionary, '/txt_line')
+    print_in_file(telephone_dictionary, '/txt_columns')
 
 
 #############
